c64c341574e96f9a75a48f70c740ecdd05fede58
marin <289024114@qq.com>
2017-04-20 23:31:43 +0800

    自动加载函数加入

---
+++ run.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 使用管理器管理异步回调
+ * 先将个回调加入到管理器中
+ *
+ * 全部回调执行完了之后(比如 数据库链接池，redis池，)，执行入口，链接server，使用swoole异步客户端连接TCPserver
+ * 数据库链接成功，redis链接成功 之后 加入到CServer管理器，CSserver主要用作消息接收，根据消息处理相应的模块业务
+ */
+include __DIR__."/bootstrap.php";
+ini_set('memory_limit','1024M');
+
+//todo mysql连接池
+
+//todo mysqlLog 连接池
+
+//todo MysqlUserCenter 连接池
+
+//todo redis连接
